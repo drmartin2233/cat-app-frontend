e7c4107e47afce86d7c8f00e56ce95b0eb112ef0
David Martin <d.r.martin.wrk@gmail.com>
2023-05-18 16:15:02 -0400

    connect backend and render

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,31 @@
-import Image from 'next/image'
+'use client';
+
 import styles from './page.module.css'
+import { useState, useEffect } from 'react'
 
 export default function Home() {
+  const [cats, setCats] = useState([]);
+
+
+useEffect(() => {
+  const getCats = async () => {
+    const response = await fetch('https://mighty-stream-52673.herokuapp.com/cats');
+    const cats =  await response.json()
+    setCats(cats);
+    console.log(response);
+  }
+
+  getCats();
+}, [])
+
   return (
     <main className={styles.main}>
-      <h1>Hello React!</h1>
+      <h1>Hello Cats!</h1>
+      {
+        cats.map(cat => (
+          <h2>{ cat.name }</h2>
+        ))
+      }
     </main>
   )
 }
