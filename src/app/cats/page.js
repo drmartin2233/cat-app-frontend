ef789b161a2f9e664c171300c9e81b4f0e3cd646
David Martin <d.r.martin.wrk@gmail.com>
2023-05-22 15:24:45 -0400

    cats and base pages

---
+++ src/app/cats/page.js
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+
+export default function Cats(){
+
+return(
+    <>
+    <Link href="/cats">On to th cats!!</Link>
+
+    <h3><Link href="/">Back to Home</Link></h3>
+    
+    </>
+)
+};
