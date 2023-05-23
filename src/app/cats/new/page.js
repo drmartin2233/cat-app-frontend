f3ad85515281aa33c286ee8618e30b40c675eeb2
David Martin <d.r.martin.wrk@gmail.com>
2023-05-23 01:48:07 -0400

    creat completed

---
+++ src/app/cats/new/page.js
@@ -0,0 +1,45 @@
+'use client'
+import { useState } from "react"
+import { useRouter } from "next/navigation"
+
+export default function NewCatForm() {
+const [newCat, setNewCat] = useState({name: "", age: 0})
+
+const router = useRouter()
+
+const handleChange = (evt) => {
+    setNewCat({...newCat, [evt.target.id]: evt.target.value})
+}
+
+
+const handleSubmit = async (evt) => {
+    evt.preventDefault()
+    const response = await fetch('https://mighty-stream-52673.herokuapp.com/cats',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(newCat),
+    })
+    const cat = await response.json()
+
+    if (cat) {
+        router.push('/cats')
+    }
+}
+
+    return(
+        <div>
+            <h1>New Cat Form</h1>
+            <form onSubmit= {handleSubmit}>
+                <label for="name">Name:
+                <input type="text" name="name" id="name" onChange={handleChange}/>
+                </label>
+                <label for="age">Age:
+                <input type="number" name="age" id="age" onChange={handleChange}/>
+                </label>
+                <input type="submit" value="Create Cat" />
+            </form>
+        </div>
+    )
+}
